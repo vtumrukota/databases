6b0a0f6f9ec6f9b1bc246287efa0de447613d795
vtumrukota <vtumrukota@gmail.com>
2015-04-17 17:37:51 -0700

    Create Schema File

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,102 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'user'
+--
+-- ---
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(30) NOT NULL DEFAULT 'Anonymous',
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT 'NULL',
+  `updatedAt` TIMESTAMP NULL DEFAULT NULL,
+  `text` MEDIUMTEXT NOT NULL DEFAULT '""',
+  `user_id` INTEGER NULL DEFAULT NULL,
+  `rooms_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+--
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(30) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`),
+KEY (`id`)
+);
+
+-- ---
+-- Table 'roomUsers'
+--
+-- ---
+
+DROP TABLE IF EXISTS `roomUsers`;
+
+CREATE TABLE `roomUsers` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
+  `room_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `user` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (rooms_id) REFERENCES `rooms` (`id`);
+ALTER TABLE `roomUsers` ADD FOREIGN KEY (user_id) REFERENCES `user` (`id`);
+ALTER TABLE `roomUsers` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `user` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `roomUsers` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `user` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `messages` (`id`,`createdAt`,`updatedAt`,`text`,`user_id`,`rooms_id`) VALUES
+-- ('','','','','','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `roomUsers` (`id`,`user_id`,`room_id`) VALUES
+-- ('','','');
 
 
 
